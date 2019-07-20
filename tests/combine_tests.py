5337af790a3b84c2993320d421245116a0645c12
Ahmad Alobaid <ahmad88me@gmail.com>
2019-07-20 21:06:52 +0200

    add test for new node

--- tests/combine_tests.py
+++ tests/combine_tests.py
@@ -143,6 +143,64 @@ class CombineTest(unittest.TestCase):
         self.assertEqual(merge_graph[target_uri]["Lc"], 0.9)  # 0.5 + 0.4
         f.close()
 
+    def test_add_multiple_bite_new_nodes(self):
+        fname = "test_volleyball_1.json"
+        fdir = os.path.join("tests", fname)
+        f = open(fdir)
+        table_name = "volleyball_double"
+        table_col = 0
+        data = {'table': table_name, 'column': table_col, 'slice': 0, 'total': 2, 'm': 3}
+        data['graph'] = (f, "volleyball.csv")
+        Bite.delete().execute()  # delete all Bites
+        Apple.delete().execute()  # delete all Apples
+        result = self.app.post('/add', data=data, content_type='multipart/form-data')
+        self.assertEqual(result.status_code, 200, msg=result.data)
+        database.connect(reuse_if_open=True)
+        self.assertEqual(len(Bite.select()), 1)
+        sleep(sleep_time)
+        result = self.app.get('/status')
+        self.assertEqual(result.status_code, 200, msg=result.data)
+        self.assertTrue(result.is_json)
+        j = {
+            "apples": [
+                {
+                    "apple": table_name,
+                    "status": STATUS_NEW,
+                    "complete": False
+                }
+            ]
+        }
+        self.assertDictEqual(result.get_json(), j)
+        fname = "test_volleyball_3.json"
+        fdir = os.path.join("tests", fname)
+        f = open(fdir)
+        data = {'table': table_name, 'column': table_col, 'slice': 1, 'total': 2, 'm': 3}
+        data['graph'] = (f, "volleyball.csv")
+        result = self.app.post('/add', data=data, content_type='multipart/form-data')
+        self.assertEqual(result.status_code, 200, msg=result.data)
+        database.connect(reuse_if_open=True)
+        self.assertEqual(len(Bite.select()), 2)
+        sleep(sleep_time)
+        result = self.app.get('/status')
+        self.assertEqual(result.status_code, 200, msg=result.data)
+        self.assertTrue(result.is_json)
+        j = {
+            "apples": [
+                {
+                    "apple": table_name,
+                    "status": STATUS_COMPLETE,
+                    "complete": True
+                }
+            ]
+        }
+        self.assertDictEqual(result.get_json(), j)
+        apple = Apple.select().where(Apple.table==table_name, Apple.column==table_col)[0]
+        f = open(os.path.join(UPLOAD_DIR, apple.fname))
+        merge_graph = json.load(f)
+        target_uri = "http://dbpedia.org/ontology/VolleyballPlayer"
+        self.assertEqual(merge_graph[target_uri]["Lc"], 0.9)  # 0.5 + 0.4
+        f.close()
+
     def test_add_multiple_incomplete_bite(self):
         fname = "test_volleyball_1.json"
         fdir = os.path.join("tests", fname)
@@ -194,3 +252,5 @@ class CombineTest(unittest.TestCase):
             ]
         }
         self.assertDictEqual(result.get_json(), j)
+
+
